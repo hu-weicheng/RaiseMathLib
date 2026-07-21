95cbd99080b475cbc8ccdc23d28cdc982d609032
Raise <huxyc@qq.com>
2026-07-21 21:01:15 +0800

    update

--- generateSine/generateSine.cpp
+++ generateSine/generateSine.cpp
@@ -25,6 +25,22 @@ std::vector<double> generateSine(
     }
     return x;
 }
+std::vector<double> generateCosine(
+    double amplitude,
+    double frequencyHz,
+    double sampleRate,
+    size_t numSamples)
+{
+    std::vector<double> x(numSamples);
+    constexpr double pi = std::numbers::pi;
+    const double omega = 2.0 * pi * frequencyHz / sampleRate;
+
+    for (size_t n = 0; n < numSamples; ++n)
+    {
+        x[n] = amplitude * std::cos(omega * n);
+    }
+    return x;
+}
 std::vector<double> generateImpulse(size_t N, size_t pos = 0)
 {
     std::vector<double> x(N, 0.0);
@@ -71,35 +87,38 @@ int main()
     constexpr double fs = 8000.0;
 
     // 单位冲击
-    auto impulse = generateImpulse(N);
+    //auto impulse = generateImpulse(N);
 
     // 正弦波
-    auto sine = generateSine(1.0, 99.6094, fs, N);
+    //auto sine = generateSine(1.0, 99.6094, fs, N);
+
+    auto cosi = generateCosine(1.0, 101, fs, N);
 
     //卷积
     //// y[n] = sum_{k=-inf}^{inf} x[k] * h[n - k]
     //y[n] = ∑_{ k = -∞ }^ {∞} x[k]·h[n - k]
-    auto y = convolve(sine, impulse);
+    //auto y = convolve(sine, impulse);
 
-    std::cout << "打印正弦型号离散序列" << std::endl;
-    for (const double& v : sine)
-    {
-        std::cout << v << std::endl;
-    }
-    std::cout << "_________________" << std::endl;
+    //std::cout << "打印正弦型号离散序列" << std::endl;
+    //for (const double& v : sine)
+    //{
+    //    std::cout << v << std::endl;
+    //}
+    //std::cout << "_________________" << std::endl;
     //for (const double& v : impulse)
     //{
     //    std::cout << v << std::endl;
     //}
-    std::cout << "打印卷积结果" << std::endl;
-    for (const double& v : y)
-    {
-        std::cout << v << std::endl;
-    }
-    std::cout << "---------------------------" << std::endl;
+    //std::cout << "打印卷积结果" << std::endl;
+    //for (const double& v : y)
+    //{
+    //    std::cout << v << std::endl;
+    //}
+    //std::cout << "---------------------------" << std::endl;
 
     // 初始化窗口（加宽，因为卷积结果更长）
     initgraph(1200, 600);
+    SetWindowText(GetHWnd(), L"DSP 信号可视化");  // 设置窗口标题
     setbkcolor(BLACK);
     cleardevice();
 
@@ -107,17 +126,22 @@ int main()
     setcolor(WHITE);
     line(0, 200, 1200, 200);
 
-    // ===== 画正弦波 =====
+    //// ===== 画正弦波 =====
+    //setcolor(YELLOW);
+    //drawSignal(sine, YELLOW, 0);
+
     setcolor(YELLOW);
-    drawSignal(sine, YELLOW, 0);
+    drawSignal(cosi, YELLOW, 0);
 
-    // ===== 画单位冲击 =====
-    setcolor(RED);
-    drawSignal(impulse, RED, 0);
+    //// ===== 画单位冲击 =====
+    //setcolor(RED);
+    //drawSignal(impulse, RED, 0);
 
-    // ===== 画卷积结果 =====
-    setcolor(GREEN);
-    drawSignal(y, GREEN, 0);
+    //// ===== 画卷积结果 =====
+    //setcolor(GREEN);
+    //drawSignal(y, GREEN, 0);
+
+
 
     _getch();
     closegraph();
